1e3585fdf06077c0539492a6b814f1d1a8a9bdf9
PrakashDev <92098897+Hackprakas@users.noreply.github.com>
2023-07-10 20:53:33 +0530

    Update iseven.java

--- iseven.java
+++ iseven.java
@@ -1,14 +1,1 @@
-public class EvenNumberChecker {
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
 
-    public static void main(String[] args) {
-        int number = 10;
-        if (isEven(number)) {
-            System.out.println(number + " is even.");
-        } else {
-            System.out.println(number + " is odd.");
-        }
-    }
-}
