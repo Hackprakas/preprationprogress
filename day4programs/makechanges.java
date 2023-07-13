f916477e80f46099105c616f050ca1d401e160c9
PrakashDev <92098897+Hackprakas@users.noreply.github.com>
2023-07-13 12:13:54 +0530

    day 3 progress(splitting of files not done)

---
+++ day4programs/makechanges.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+public int uniqueDigitsCount(int input1){
+
+int count[]={0,0,0,0,0,0,0,0,0,0};
+        int unique=1;
+        while(input1>0){
+            int n=input1%10;
+            count[n]++;
+            input1=input1/10;
+        }
+        for(int i=0;i<10;i++){
+            if(count[i]==1){
+                unique=unique+1;
+            }
+        }
+		// throw new UnsupportedOperationException("uniqueDigitsCount(int input1)");
+        return unique;
+	}
+}
+
+
+
+
+
+import java.io.*;
+import  java.util.*;
+public int uniqueDigitsCount(int input1){
+String numStr = Integer.toString(input1);
+    int[] digitCounts = new int[10];
+    for (int i = 0; i < numStr.length(); i++) {
+        int digit = Character.getNumericValue(numStr.charAt(i));
+        digitCounts[digit]++;
+    }
+    int count = 0;
+    for (int i = 0; i < digitCounts.length; i++) {
+        if (digitCounts[i] == 1) {
+            count++;
+        }
+    }
+    return count;
